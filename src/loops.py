7ed229f5c5cc0a44173157754e5023ca7601a681
Lars S <>
2023-09-20 17:46:42 -0700

    Detailed my next plans out.

--- src/loops.py
+++ src/loops.py
@@ -8,5 +8,21 @@ while True:
     x = 44   #$
 
 becomes illegal. I will notate all my thoughts and plans for developing
-that feature in this scratch file.    
-'''
+that feature in this scratch file.
+
+I think this will be pretty simple, we need a flag to determine if we are in a loop,
+and we handle loop scopes just like function/class scopes. Basically all assignments
+to constant inside a loop are illegal. So if we modify some of the state tools to track
+loops and exit loops when indentation decreases (these tools already exist for scopes)
+then all we need to do is through a loop check anytime a constant is found, and print
+a unique message when a constant is found in a loop. That's the entire spice. I could
+probably do it in 4-8 line changes total. 
+
+We need a way to track sub loops, my initial thought of a single bool fails because you need
+to track how many loops deep you are and what whitespace triggered each loop. Not to different
+from the names_in_scope object but without the names. Maybe just the type of loop for/while as
+a name then it would look like while.for.while etc. This won't be used as keys but having the
+object will be nice for debugging or adding more detailed error messages in the future (unlikely).
+
+When that list of loops is empty we pass the 'not in a loop' test. So that one data object and a
+few extra pieces of logic will enable this api.'''
--- src/strings.py
+++ src/strings.py
@@ -2,8 +2,34 @@
 
 '''This file is where I will think out and plan string awareness
 for icebrakes. Icebrakes needs to know that lines like this
-"foo = 44 " are not an assignment. I will have to take nested strings
-into account.
+"foo = 44 " are not an assignment. 
 
-I will also need to be concerned with doc strings/multi line comments.
-'''
+I will have to take nested strings into account.  (DO I?)
+
+Here is how I see the string problem. Multi line strings are the biggest deal
+because they invalidate entire lines. So I need some kind of flag that
+sets a multiline string, and we just skip every line until that flag is unset. 
+
+We don't need the names from those lines but we do care about parsing for the 
+three quote mark pattern to end the multiline string.
+
+As for single line strings we only need to worry about equal line parse
+and I think it might be abstractly simple in that we only char if ' or "
+occur before the equal sign.
+
+'word' = 5   # illegal
+
+That's invalid python anyways, mostly for assignment the equal sign goes left
+so single strings might be a very simple task. (for the purposes of this linter)
+
+These two modes are basically entirely different parts of the code I need to edit
+and so it might make sense to do it in two patches. Idk I kind of want to avoid
+a weird string limbo where things half work.
+
+I need a few vars to track the beginning and ended of every """ triple quote.
+Or maybe only the outermost triple quote at a time. This """ doesn't mean
+anything to python because it's already a string.
+
+I guess we need to check every line for triple quotes and when we are in a multi
+line string we need the leftmost match. We also care if a trip quote appears twice
+is the same line so we actually need to parse the entire line.'''
