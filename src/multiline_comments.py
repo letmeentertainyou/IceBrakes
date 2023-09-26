b6cfb46503a5396798f72f578b1ab8246dad7a8e
Lars S <>
2023-09-25 21:04:40 -0700

    Cleaned up src before new features.

--- src/multiline_comments.py
+++ src/multiline_comments.py
@@ -9,4 +9,31 @@ is exited.
 
 This is going to be a nice performance boost because it allows us to ignore
 many lines we checked before.
+
+There can only be one multiline string at a time so we only need to track
+whether it was single or double quotes that entered the string and once
+a string is exited we need to continue tracking.
+
+It might be best to use regex to get every occurrence of """ from the line
+and then do some logic based on the tree of """s. We need to track groups
+of three quotes at a time, and track the index of the third quote. So that
+a pattern line """" doesn't appear as an entrance and an exit. 
+
+Also """" "" is not a valid exit either. It needs to be another group of
+three quotes like """""" or """ """
+
+If there are characters before the string starts then we do need to parse
+the line. So this is mostly for tracking when entire sections of a file
+are not to be parsed.
+
+
+
+    1. If string is declared set flag
+    2. Parse the rest of the string to possibly unset the flag too
+    3. If index of string is zero continue.
+
+
 '''
+
+# Parsing something like this will require an engine.
+# ''' """ ''' """ ''' """
