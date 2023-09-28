0ac1699f48cdc2c7b1056ecc37fcdc77cb74448a
Lars S <>
2023-09-27 18:00:51 -0700

    Laid out most of the API for multiline comment parsing, will merge it in the next push.

--- src/multiline_comments.py
+++ src/multiline_comments.py
@@ -27,13 +27,68 @@ the line. So this is mostly for tracking when entire sections of a file
 are not to be parsed.
 
 
-
     1. If string is declared set flag
     2. Parse the rest of the string to possibly unset the flag too
     3. If index of string is zero continue.
-
-
 '''
+from typing import List, Tuple
 
-# Parsing something like this will require an engine.
-# ''' """ ''' """ ''' """
+def find_first_index(tag: str, line: str) -> Tuple:
+    '''Grabs the index of the first occurrence of a given string.'''
+    try:
+        return line.index(tag), tag
+    except ValueError:
+        return None, None
+
+
+# LATER open_comment will be States.open_comment
+#  open_comment
+# -1 = """
+#  0 = False
+#  1 = '''
+def parse_line(line: str, open_comment: int = 0) -> None:
+    '''A basic multiline comment parser. Still needs a bit of recursion.'''
+    single: str = "'''"
+    double: str = '"""'
+
+    tags: List[str] = []
+
+    # Both of these proc on zero. Chef's kiss!
+    if single in line and open_comment >=0:
+        tags.append(single)
+
+    if double in line and open_comment <=0:
+        tags.append(double)
+
+    if tags:
+        all_tags: List[Tuple] = [find_first_index(tag, line) for tag in tags]
+        smallest = min(all_tags)
+
+        if smallest[1] == single:
+            open_comment = 1
+
+        if smallest[1] == double:
+            open_comment = -1
+
+        print(smallest)
+
+        # Pop off line
+        # use the index we just got
+
+        # Recurse
+        # send the truncated string back through.
+        # It will stop looping when no more tags are found in the line.
+
+    # if tags is empty then the line is done being parsed.
+
+
+# This is a demo of how this would be implemented,
+# We'll need to sneak a continue in there somewhere too.
+
+if __name__ == '__main__':
+    with open('multiline_trainer.txt', 'r', encoding='UTF-8') as tempfile:
+        file: List[str] = tempfile.readlines()
+
+    # pylint didn't line 'line' here
+    for item in file:
+        parse_line(item)
